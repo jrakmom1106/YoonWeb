8bce8b4a724cbef2ab578cb20ad7141558a5dcde
jrakmom1106 <jrakmom971106@gmail.com>
2022-12-06 10:05:48 +0900

    revert

--- yoonweb/bin/src/main/webapp/WEB-INF/views/_stylesheet.jsp
+++
@@ -1,3 +0,0 @@
-<link type="text/css" rel="stylesheet" href="/assets/css/common.css?ver=1"/>
-<link type="text/css" rel="stylesheet" href="/assets/css/ui-style.css?ver=1"/>
-
--- yoonweb/bin/src/main/webapp/assets/lib/ajax/test.php
+++
@@ -1,5 +0,0 @@
-
-$name = (isset($_POST['userName'])) ? $_POST['userName'] : 'no name';
-$computedString = "안녕, " . $name . "!";
-$array = ['userName' => $name, 'computedString' => $computedString];
-echo json_encode($array);
